e91839fe244982c53d24a0b2599b290419492f2d
Greenstick <benacordier@gmail.com>
2023-04-05 17:44:21 -0700

    refactor 🎨: sankey class

--- src/modules/visualizations/charts/sankey.js
+++ src/modules/visualizations/charts/sankey.js
@@ -6,178 +6,134 @@ import * as D3 from "d3";
 import * as D3Sankey from "d3-sankey";
 import Chart from "../chart.js";
 
-/*
-Defaults
-*/
-
-const _defaults = {
-  id: "#overview-sankey",
-  width: 1200,
-  height: 540,
-  margin: {
-    top: 40,
-    left: 40,
-    right: 40,
-    bottom: 40
-  },
-  palette: [
-    "#540d6e",
-    "#ee4266",
-    "#ffd23f",
-    "#3bceac",
-    "#0ead69"
-  ],
-  node: {
-    width: 120,
-    padding: 40,
-    class: "node",
-    alignment: "justify",
-    stroke: "transparent"
-  },
-  link: {
-    class: "link"
-  }
-}
 
 /*
-Prototype
+Sankey Chart Class
 */
 
-// Overview Visualization Entry Point
-var Sankey = function (config) {
-    return this.__init__(config);
-};
+class SankeyChart extends Chart {
 
-// Overview Visualization Methods
-Sankey.prototype = {
-
-  _node_alignment_map : {
+  // References
+  #alignMap    = {
     "left": D3Sankey.sankeyLeft,
     "right": D3Sankey.sankeyRight,
     "center": D3Sankey.sankeyCenter,
     "justify": D3Sankey.sankeyJustify
-  },
+  };
 
-  // Default config
+  constructor (config) {
 
-  __init__ : function (config) {
-    var self = this;
+    // Configure Parent
+    super(config);
 
-    self.id           = config.id;
-    self.width        = config.width;
-    self.height       = config.height;
-    self.margin       = config.margin;
-    self.palette      = config.palette;
-    self.node         = config.node;
-    self.link         = config.link;
-    self.ncols        = config.ncols;
-    self.data         = config.data;
+    // Configure Sankey Chart
+    this.opacity      = config.opacity;
+    this.node         = config.node;
+    this.link         = config.link;
 
-    self.uid          = self._uid();
-    self.nodeAlign    = self._node_alignment_map[self.node.alignment];
-    self.linkMethod   = D3Sankey.sankeyLinkHorizontal();
-    self.inner        = {
-      height: self.height - self.margin.top - self.margin.bottom,
-      width: self.width - self.margin.left - self.margin.right
-    };
+    // Computed References
+    this.nodes        = [];
+    this.links        = [];
+    this.targets      = [];
+    this.sources      = [];
+    this.nodeAlign    = this.#alignMap[this.node.alignment];
+    this.linkMethod   = D3Sankey.sankeyLinkHorizontal();
 
-    return self.update();
+    return this;
 
-  },
+  }
 
-  update: function () {
-    var self = this;
+  update () {
 
-    /*
-    Graph Setup
-    */
-
-    self.svg = D3.select(self.id)
+    this.svg = D3.select(this.id)
       .classed("sankey-chart", true);
 
-    self.color = D3.scaleOrdinal(self.palette);
+    this.data = this.#mapData(this.data);
 
-    self.graph = D3Sankey.sankey(self.data)
+    this.graph = D3Sankey.sankey()
       .nodeId(d => d.name)
-      .nodeAlign(self.nodeAlign)
-      .nodeWidth(self.node.width)
-      .nodePadding(self.node.padding)
-      .size([self.width, self.height])
-      .extent([[self.margin.left, self.margin.top], [self.width - self.margin.right, self.height - self.margin.bottom]])
-      .nodes(self.data.nodes)
-      .links(self.data.links)
-      .nodeSort(null)(self.data);
-
-    // self.graph = self.graph(self.data);
+      .nodeAlign(this.nodeAlign)
+      .nodeWidth(this.node.width)
+      .nodePadding(this.node.padding)
+      .size([this.width, this.height])
+      .extent([[this.margin.left, this.margin.top], [this.width - this.margin.right, this.height - this.margin.bottom]])
+      .nodes(this.data.nodes)
+      .links(this.data.links)(this.data);
 
     // Layout sorting
-    self.nodeSort = self.node.sort !== null ? self.graph.nodes.sort((a, b) => D3[self.node.sort](a.value, b.value)) : null;
-    self.linkSort = self.link.sort !== null ? self.graph.links.sort((a, b) => D3[self.link.sort](a.value, b.value)) : null;
+    this.nodeSort = this.node.sort !== null ? this.graph.nodes.sort((a, b) => D3[this.node.sort](a.value, b.value)) : null;
+    this.linkSort = this.link.sort !== null ? this.graph.links.sort((a, b) => D3[this.link.sort](a.value, b.value)) : null;
 
     /*
     Define Gradients
     */
 
-    self.gradients = self.svg
-      .append("defs")
+    this.color = D3.scaleOrdinal(this.palette);
+
+    this.gradients = this.svg.append("defs")
+        .classed("gradient-defs", true)
+        .attr("id", `gradient-defs_${this.uid}`)
         .style("mix-blend-mode", "multiply")
         .selectAll("linearGradient")
-        .data(self.graph.links)
+        .data(this.graph.links)
         .enter()
           .append("linearGradient")
-            .attr("id", d => `${self.uid}_gradient_${self._rename(d.source.name)}_${self._rename(d.target.name)}`)
+            .classed("gradient", true)
+            .attr("id", d => `${this.uid}_gradient_${this.tokenize(d.source.name)}_${this.tokenize(d.target.name)}`)
             .attr("gradientUnits", "userSpaceOnUse")
             .attr("x1", d => d.source.x1)
             .attr("y1", d => Math.max(d.source.y0, d.target.y0, d.source.y1, d.target.y1))
             .attr("x2", d => d.target.x0)
             .attr("y2", d => Math.max(d.source.y0, d.target.y0, d.source.y1, d.target.y1));
 
-    self.gradientsource = self.gradients
+    this.gradientsource = this.gradients
       .append("stop")
         .attr("offset", "5%")
-        .attr("stop-color", d => d.color = self.color(self._rename(d.source.name)))
-        .attr("stop-opacity", self.link.opacity);
+        .attr("stop-color", d => d.color = this.color(this.tokenize(d.source.name)))
+        .attr("stop-opacity", this.link.opacity);
 
-    self.gradienttarget = self.gradients
+    this.gradienttarget = this.gradients
       .append("stop")
         .attr("offset", "95%")
-        .attr("stop-color", d => d.color = self.color(self._rename(d.target.name)))
-        .attr("stop-opacity", self.link.opacity);
+        .attr("stop-color", d => d.color = this.color(this.tokenize(d.target.name)))
+        .attr("stop-opacity", this.link.opacity);
 
     /*
     Generate Data Elements
     */
 
-    self.nodes = self.svg
-      .append("g")
-        .classed(`${self.node.class}s`, true)
+    this.nodes = this.svg.append("g")
+        .classed(`${this.node.class}s`, true)
+        .attr("id", `nodes_${this.uid}`)
+        .attr("transform", `translate(${this.margin.left}, ${this.margin.top})`)
         .selectAll("rect")
-        .data(self.graph.nodes)
+        .data(this.graph.nodes)
         .enter()
           .append("rect")
-            .classed(self.node.class, true)
+            .classed(this.node.class, true)
             .attr("x", d => d.x0)
             .attr("y", d => d.y0)
             .attr("width", d => d.x1 - d.x0)
             .attr("height", d => d.y1 - d.y0)
-            .attr("fill", d => self.color(self._rename(d.name)))
-            .attr("opacity", self.node.opacity)
+            .attr("fill", d => this.color(this.tokenize(d.name)))
+            .attr("opacity", this.node.opacity)
             .style("shape-rendering", "crispEdges");
 
-    self.links = self.svg
-      .append("g")
-        .classed(`${self.link.class}s`, true)
+    this.links = this.svg.append("g")
+        .classed(`${this.link.class}s`, true)
+        .attr("id", `links_${this.uid}`)
+        .attr("transform", `translate(${this.margin.left}, ${this.margin.top})`)
         .selectAll("path")
-        .data(self.graph.links)
+        .data(this.graph.links)
         .enter("path")
           .append("path")
-            .classed(self.link.class, true)
-            .attr("d", d => d.path = self.linkMethod(d))
+            .classed(this.link.class, true)
+            .attr("d", d => d.path = this.linkMethod(d))
             .attr("fill", "none")
-            .attr("stroke", d => `url(#${self.uid}_gradient_${self._rename(d.source.name)}_${self._rename(d.target.name)})`)
+            .attr("stroke", d => `url(#${this.uid}_gradient_${this.tokenize(d.source.name)}_${this.tokenize(d.target.name)})`)
             .attr("stroke-width", d => d.width)
             .attr("stroke-linecap", "square")
-            .attr("opacity", self.link.opacity)
+            .attr("opacity", this.link.opacity)
             .attr("vector-effect", "none")
             .style("shape-rendering", "geometricPrecision");
 
@@ -185,35 +141,51 @@ Sankey.prototype = {
     Generate Labels
     */
 
-    self.nodelabels = self.svg
-      .append("g")
-        .classed(`${self.node.class}-labels`, true)
+    this.nodelabels = this.svg.append("g")
+        .classed(`${this.node.class}-labels`, true)
+        .attr("id", `node-labels_${this.uid}`)
+        .attr("transform", `translate(${this.margin.left}, ${this.margin.top})`)
         .selectAll("text")
-        .data(self.graph.nodes)
+        .data(this.graph.nodes)
         .join("text")
           .text(d => `${d.name} (${d.value})`)
-            .classed(`${self.node.class}-label`, true)
-            .attr("id", d =>  `${self.uid}_node-label_${self._rename(d.name)}`)
-            .attr("font-size", self.node.fontsize)
+            .classed(`${this.node.class}-label`, true)
+            .attr("id", d =>  `${this.uid}_node-label_${this.tokenize(d.name)}`)
+            .attr("font-size", this.node.fontsize)
             .attr("x", d => d.x0)
-            .attr("y", d => d.y0 - (self.node.fontsize / 2));
+            .attr("y", d => d.y0 - (this.node.fontsize / 2));
 
-    return self;
+    return this;
 
-  },
+  }
 
-  debug: function () {
-    let self = this;
-    console.log(self);
-    return self;
-  },
+  #mapData (data) {
 
-  _uid: function () {
-    return `O-${Math.random().toString(16).slice(2, 8)}`
-  },
+    let mapped = [];
 
-  _rename: function (name) {
-    return (typeof(name) === "string") ? name.replace(/\s/g, "-").toLowerCase() : "";
+    // Set Links
+    data.forEach(row => {
+      this.links.push({
+        target : super.asType(this.accessors.target.type, row[this.accessors.target.key]),
+        source : super.asType(this.accessors.source.type, row[this.accessors.source.key]),
+        value  : super.asType(this.accessors.value.type, row[this.accessors.value.key])
+      })
+    });
+
+    // Set Nodes
+    this.targets = super.getUniqueKeys(this.links, this.accessors.target.key);
+    this.sources = super.getUniqueKeys(this.links, this.accessors.source.key);
+    this.nodes = Array.from(new Set(this.targets.concat(this.sources)));
+
+    mapped = {
+      nodes: this.nodes.map(
+        node => {return {name: node}}
+      ),
+      links: this.links
+    };
+
+    return mapped;
+
   }
 
 };
@@ -222,4 +194,4 @@ Sankey.prototype = {
 Exports
 */
 
-export default Sankey;
+export default SankeyChart;
